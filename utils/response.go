25208ca6737432e23dca83e64dd53ffd1439fddf
Wasim Akram <wasim@qburst.com>
2020-08-23 01:30:26 +0530

    Summary :: Setting Response 1. Added functions for setting error and success responses.

---
+++ utils/response.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// CustomError : The error format in api response
+type CustomError struct {
+	Error string `json:"error"`
+}
+
+// Send : General function to send api response
+func Send(w http.ResponseWriter, status int, payload interface{}) {
+	result, err := json.Marshal(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(result)
+}
+
+// Fail : General function to send api response
+func Fail(w http.ResponseWriter, status int, details string) {
+	response := &CustomError{
+		Error: details,
+	}
+	result, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(result)
+}
